87a859a1488f3ac8bf91e856c46c018481f29c1a
Till Hackler <till.hackler@gmx.com>
2016-05-06 12:06:28 +0100

    Fixed: Early calls to Wire library prevent functioning        of 8x8 matrices, Serial and (potentially) DS3231        clock
    * A global DS3231 instance is created before setup()
    * In the old code this called Wire.begin() and set the
      time to a default, using calls to Wire.beginTransmission()
      and Wire.write()
    * These early calls (before setup()) broke serial comms
      with the Arduino Nano via the Serial class and they
      broke comms with the DotMatrix class (which is built on
      LedControl)
    * This commit fixes this issue by postponing setting a time
      to within the setup() call
    * Now Serial, DS3231 and DotMatrix work

--- AlarmClock/AlarmClock.ino
+++ AlarmClock/AlarmClock.ino
@@ -1,8 +1,7 @@
 /*  AlarmClock.ino
  *   
  *  A simple alarm clock using a DS3231 clock chip and three 8x8
- *  dot matrix displays to show the current time, set and (de)
- *  activate an alarm.
+ *  dot matrix displays to show the current time, and set an alarm.
  *  
  *  Created by Till Hackler, December 2015
  */
@@ -12,101 +11,119 @@
  *  Including all libraries that are ever used inside the     
  *  project is necessary here if using the Arduino IDE for
  *  making the executable, because its linker links only
- *  files that are included somewhere within the present file.
+ *  files that are included somewhere within the this file.
  */
 #include "Wire.h"
-#include "Encoder.h"
+//#include "Encoder.h"
 #include "LedControl.h"
-#include "Loudspeaker.h"
+//#include "Loudspeaker.h"
 #include "DotMatrix.h"
 #include "DS3231.h"
 
 ///////////////////
+// Sign function from http://stackoverflow.com/a/4609795
+///////////////////
+//template <typename T> int sgn(T val) {
+//    return (T(0) < val) - (val < T(0));
+//}
+
+///////////////////
 // Global variables
 ///////////////////
-enum action {displayCurrentTime, displayAlarmTime, toggleAlarm};
-action clockAction = displayCurrentTime;
-bool alarmIsOn = false;
-int lastEventTime = -1;
+//enum action {displayCurrentTime, displayAlarmTime, toggleAlarm};
+//action clockAction = displayCurrentTime;
+//bool alarmIsOn = false;
+//int lastEventTime = -1;
+//
+//struct aTime
+//{
+//    uint8_t h = 0; // hours
+//    uint8_t m = 0; // minutes
+//};
+//
+//aTime currentTime, alarmTime;
 
-struct aTime
-{
-    uint8_t h = 0; // hours
-    uint8_t m = 0; // minutes
-};
-
-aTime currentTime, alarmTime;
-
-
+DotMatrix   matrix;
 DS3231 clk;
+
 // actual date
-byte dateSecond,
-     dateMinute,
-     dateHour,
-     dateDayOfWeek,
-     dateDayOfMonth,
-     dateMonth,
-     dateYear;
+//byte dateSecond,
+//     dateMinute,
+//     dateHour,
+//     dateDayOfWeek,
+//     dateDayOfMonth,
+//     dateMonth,
+//     dateYear;
 
 
-//DotMatrix   matrix;
 
-Encoder       enc(2, 5);
-long          oldPosition  = 0;
-unsigned long encoderLast = 0;
-unsigned long fastRotThres = 100000; // in us
 
-Loudspeaker ls;
+//Encoder       enc(2, 5);
+//long          oldPosition  = 0;
+//unsigned long encoderLast = 0;
+//unsigned long fastRotThres = 100000; // in us
+
+//Loudspeaker ls;
 
 /////////////////////////////
 // Encoder callback functions
 /////////////////////////////
-void buttonPressed()
-{}
-
-void buttonDepressed()
-{}
-
+//void buttonPressed()
+//{}
+//
+//void buttonDepressed()
+//{}
 
 /* Initialise classes
  * 
  */
-LedControl lc2 = LedControl(4, A1, A2, 3);
-
 void setup() {
-
-for(int8_t i = 0; i < 3; i++)
-{
-    lc2.clearDisplay(i);
-    lc2.setIntensity(i, 1);
-    lc2.shutdown(i, false);
-   
-}
-  
-    clk.setTime(0, 1, 1, 1, 1, 1, 0); // set current time for production // IDEA / TODO: Try setting current computer system time through clever use of preprocessor logic
-//    matrix.setup(4, A1, A2, 3); 
-    ls.setPin(A3);
-//    matrix.displayTime(2,2);
+    clk.setTime(0, 1, 1, 1, 1, 1, 0); // set time when programming the clock
+    Serial.begin(9600);
     
+    Serial.println("HELLO");
+    
+    matrix.setup(4, A1, A2, 3); 
+//    ls.setPin(A3);
+    matrix.displayTime(00,00);
 }
 
 void loop() {
-  lc2.setLed(1,1,1,true);
+//  int positionDelta = 0;
+//   handle encoder rotation
+//  long newPosition = enc.read() / 4;
+//  if (newPosition != oldPosition) {
+//    unsigned long timeNow = micros();
+//    unsigned long timeDelta = timeNow - encoderLast;
+//    positionDelta = newPosition - oldPosition;
+//    if (timeDelta < fastRotThres) {
+//      positionDelta *= 5;
+//    }
+//    oldPosition = newPosition;
+//    encoderLast = timeNow;
+//  }
+  
   
 //  switch(clockAction)
 //  {
 //      case displayCurrentTime:
-//          matrix.displayTime(2, 20);
+//        if (positionDelta != 0) {
+//          
+//          if (currentTime.m + positionDelta > 59) { currentTime.h = (currentTime.h + 1) % 24; }
+//          if (currentTime.m + positionDelta <  0) { currentTime.h = (currentTime.h - 1 + 24) % 24; }
+//          
+//          currentTime.m = (currentTime.m + positionDelta + 60) % 60;
+//          
+////          matrix.displayTime(currentTime.h, currentTime.m);
+//        }
 //      case displayAlarmTime:
+//        if (positionDelta != 0) {
 //          break;
+//        }
 //      case toggleAlarm:
 //          break;
 //  }
-//    clk.readTime(&dateSecond,
-//                 &dateMinute,
-//                 &dateHour,
-//                 &dateDayOfWeek,
-//                 &dateDayOfMonth,
-//                 &dateMonth,
-//                 &dateYear);
+  delay(500);
+  clk.printTime();
+  Serial.println("Hello");   
 }

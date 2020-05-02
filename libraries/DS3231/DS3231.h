f507b7b0540c6d190793d21a5667fa913ff6b3b3
Till Hackler <till.hackler@gmx.com>
2020-05-02 15:30:52 +0100

    Allow setting and reading alarm time on DS3231

--- libraries/DS3231/DS3231.h
+++ libraries/DS3231/DS3231.h
@@ -16,8 +16,10 @@
 
 #include "Arduino.h"
 
-#define DS3231_I2C_ADDRESS 0x68
 #define SERIAL_BAUD_RATE 9600
+#define DS3231_I2C_ADDRESS 0x68
+#define DS3231_REG_SECONDS 0x00
+#define DS3231_REG_ALARM_SECONDS 0x07
 
 class DS3231
 {
@@ -44,13 +46,50 @@ class DS3231
                       uint8_t *dayOfMonth,
                       uint8_t *month,
                       uint8_t *year);
+        void setAlarmTime(uint8_t second,
+                          uint8_t minute,
+                          uint8_t hour,
+                          uint8_t dayOfWeek,
+                          uint8_t dayOfMonth,
+                          uint8_t month,
+                          uint8_t year);
+        void readAlarmTime(uint8_t *second,
+                           uint8_t *minute,
+                           uint8_t *hour,
+                           uint8_t *dayOfWeek,
+                           uint8_t *dayOfMonth,
+                           uint8_t *month,
+                           uint8_t *year);
         void readHourMinute(uint8_t *minute,
                             uint8_t *hour);
         void printTime();
+        void printAlarmTime();
     private:
         uint8_t decToBcd(uint8_t val);
         uint8_t bcdToDec(uint8_t val);
-
+        void setTimeAtAddr(uint8_t addr,
+                           uint8_t second,
+                           uint8_t minute,
+                           uint8_t hour,
+                           uint8_t dayOfWeek,
+                           uint8_t dayOfMonth,
+                           uint8_t month,
+                           uint8_t year);
+        void readTimeAtAddr(uint8_t addr,
+                            uint8_t *second,
+                            uint8_t *minute,
+                            uint8_t *hour,
+                            uint8_t *dayOfWeek,
+                            uint8_t *dayOfMonth,
+                            uint8_t *month,
+                            uint8_t *year);
+        void timeToSerial(uint8_t second,
+                          uint8_t minute,
+                          uint8_t hour,
+                          uint8_t dayOfWeek,
+                          uint8_t dayOfMonth,
+                          uint8_t month,
+                          uint8_t year);
 };
 
 #endif // DS3231_h
